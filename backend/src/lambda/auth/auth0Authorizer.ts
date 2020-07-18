0301607c1db222af24a3ec8838d1d00f033b3070
Anil kumar Nandamuri <anilkumarnandamuri@Anils-MacBook-Pro.local>
2020-07-17 20:49:31 -0700

    Refactored the code to make it clean

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -24,7 +24,7 @@ export const handler = async (
     return {
       principalId: jwtToken.sub,
       policyDocument: {
-        Version: '2012-10-17',
+        Version: '2012-10-17', 
         Statement: [
           {
             Action: 'execute-api:Invoke',
